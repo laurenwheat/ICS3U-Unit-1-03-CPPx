6788ce0ce2b2a1d6688cdeb107847cc924a6b235
Ubuntu <ubuntu@ip-172-31-59-214.ec2.internal>
2021-06-17 15:27:02 +0000

    did hellp world

---
+++ hello_worldx.cpp
@@ -0,0 +1,12 @@
+// Copyright Lauren Wheatley
+//
+// Created by Lauren Wheatley
+// Created on June 2021
+// This program prints hello world
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!";
+    return 0;
+}
